ce26e064c0defb5723a1c151ca63e45ffa80b886
jeansiufu@gmail.com <jeansiufu@gmail.com@cc968001-bd66-2d6e-ce54-c8733718f4f8>
2014-07-29 20:00:59 +0000

    Pacote NoveltyDetector adcionado.

--- moa-tcc/src/moa/noveltydetector/AbstractNoveltyDetector.java
+++ moa-tcc/src/moa/noveltydetector/AbstractNoveltyDetector.java
@@ -1,11 +1,21 @@
 package moa.noveltydetector;
 
+import moa.clusterers.Clusterer;
+import moa.options.AbstractOptionHandler;
 import weka.core.Instance;
 
-public abstract class AbstractNoveltyDetector {
+public abstract class AbstractNoveltyDetector extends AbstractOptionHandler
+	implements Clusterer{
 
 	
 	public abstract void trainOnInstanceImpl(Instance inst);
+	
+
+	@Override
+	public Clusterer copy() {
+		return (Clusterer) super.copy();
+	}
+
 	
 	//public abstract trainOnffline();
 }
